c0bc11575f7ffc03501fce44b83a44e5ade945e1
HenKLM <jlucasleite79@gmail.com>
2026-06-11 17:33:00 -0300

    Create Atv Check.c

---
+++ Atv Check.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    char nome[100];
+    float nota1, nota2, media;
+
+    printf("Nome do aluno: ");
+    fgets(nome, sizeof(nome), stdin);
+
+    printf("Primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Segunda nota: ");
+    scanf("%f", &nota2);
+
+    media = (nota1 + nota2) / 2;
+
+    // Exibição do resultado
+    printf("\nAluno: %s", nome);
+    printf("Média: %.2f\n", media);
+
+    if (media >= 7) {
+        printf("Resultado: Aprovado\n");
+    } else if (media >= 5) {
+        printf("Resultado: Recuperação\n");
+    } else {
+        printf("Resultado: Reprovado\n");
+    }
+
+    return 0;
+}
